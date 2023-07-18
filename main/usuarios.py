49581ff3a228e8d5593a307e8488419829619c42
Diego <dekage66@gmail.com>
2023-07-18 12:38:56 -0700

    mejor estructura

---
+++ main/usuarios.py
@@ -0,0 +1,24 @@
+class Usuario:
+    def __init__(self, id, nombre, apellido, historial_eventos=None):
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellido
+        if historial_eventos is None:
+            self.historial_eventos = []
+        else:
+            self.historial_eventos = historial_eventos
+    
+    def __str__(self):
+        return f"{self.nombre}"
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "historial_eventos": self.historial_eventos
+        }
+        
+    @classmethod
+    def from_json(cls, data):
+        return cls(data["id"], data["nombre"], data["apellido"], data["historial_eventos"])
